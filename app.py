5800b8e55df4298fb0f23e34aa5f9ed6e9fd7e0f
ShrutiJain143 <83031894+ShrutiJain143@users.noreply.github.com>
2021-05-11 14:28:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,57 +16,13 @@ X = dataset.iloc[:,1:10].values
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values= np.NAN, strategy= 'constant', fill_value='Female', verbose=1, copy=True)
 #Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 1:2])
+imputer = imputer.fit(X[:, 2:3]) 
 #Replacing missing data with the calculated mean value  
-X[:, 1:2]= imputer.transform(X[:, 1:2])  
+X[:, 2:3]= imputer.transform(X[:, 2:3])  
+
 
 # Taking care of missing data
-#handling missing data (Replacing missing data with the mean value)  
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values= np.NAN, strategy= 'constant', fill_value='Female', verbose=1, copy=True)
-#Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 2:3])
-#Replacing missing data with the calculated mean value  
-X[:, 2:3]= imputer.transform(X[:, 2:3])
-
-# Taking care of missing data
-#handling missing data (Replacing missing data with the mean value)  
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values= np.NAN, strategy= 'mean', fill_value=None, verbose=1, copy=True)
-#Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 3:9])
-#Replacing missing data with the calculated mean value  
-X[:, 3:9]= imputer.transform(X[:, 3:9])
-
-# Taking care of missing data
-#handling missing data (Replacing missing data with the mean value)  
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values= np.NAN, strategy= 'most_frequent', fill_value='none', verbose=1, copy=True)
-#Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 6:8])
-#Replacing missing data with the calculated mean value  
-X[:, 6:8]= imputer.transform(X[:, 6:8])
-
-# Taking care of missing data
-#handling missing data (Replacing missing data with the mean value)  
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values= np.NAN, strategy= 'mean', fill_value=None, verbose=1, copy=True)
-#Fitting imputer object to the independent variables x.   
-imputer = imputer.fit(X[:, 8:9])
-#Replacing missing data with the calculated mean value  
-X[:, 8:9]= imputer.transform(X[:, 8:9])  
-
-# Encoding Categorical data:
-# Encoding the Independent Variable
-from sklearn.preprocessing import LabelEncoder
-labelencoder_X = LabelEncoder()
-X[:, 1] = labelencoder_X.fit_transform(X[:,1])
-
-# Encoding Categorical data:
-# Encoding the Independent Variable
-from sklearn.preprocessing import LabelEncoder
-labelencoder_X = LabelEncoder()
-X[:, 1] = labelencoder_X.fit_transform(X[:,2])
+#handling missing d
 
 
 
